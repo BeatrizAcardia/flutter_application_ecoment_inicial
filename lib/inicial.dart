ba1e733fc0627f643c1a2b184397c674752144c6
BeatrizAcardia <beatrizcardia53@gmail.com>
2024-05-07 20:51:27 -0300

    commit 7/5/24

--- lib/inicial.dart
+++ lib/inicial.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
 
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 class Myinicial extends StatefulWidget {
@@ -10,44 +11,83 @@ class Myinicial extends StatefulWidget {
 }
 
 class _MyinicialState extends State<Myinicial> {
+
   @override
 
+  final onda = SizedBox(
+    child: Image.asset('assets/imgs/ondinhaverdinha.png'),
+  );
+
+List <String> images = [ "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTIZccfNPnqalhrWev-Xo7uBhkor57_rKbkw&usqp=CAU" , " https://wallpaperaccess.com/full/2637581.jpg" ] ;   
 
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Color.fromARGB(255, 78, 158, 90),
-        leading: Image.asset('assets/imgs/logo.png', height: 350, width: 350,),
+        leading: Image.asset(
+          'assets/imgs/logo.png',
+          height: 350,
+          width: 350,
         ),
-        body: SingleChildScrollView(
+      ),
+      
+      body: SingleChildScrollView(
           child: Column(
-            children: [
-
-              DecoratedBox(decoration: BoxDecoration(
-              color: Color.fromARGB(255, 78, 158, 90)),
-              child: 
-              Padding(
+        children: [
+          DecoratedBox(
+            decoration: BoxDecoration(color: Color.fromARGB(255, 78, 158, 90)),
+            child: Padding(
                 padding: EdgeInsets.all(30),
-                child: 
-                Column(children: [
-                  Text("Reciclar e reutilizar, não deixe o momento passar!", style: TextStyle(color: Colors.white, fontSize: 35, fontFamily: 'Nunito', fontWeight: FontWeight.bold), textAlign: TextAlign.center),
-                  SizedBox(height: 10),
-                  Text("Aqui as suas ideias são salvadoras.", style: TextStyle(color: Colors.white, fontFamily: 'Nunito', fontWeight: FontWeight.bold),),
-                  SizedBox(height: 10,),
-                  ElevatedButton(onPressed: () {
-                    setState(() {
-                      
-                    });
+                child: Column(
+                  children: <Widget>[
+                    Text("Reciclar e reutilizar, não deixe o momento passar!",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 35,
+                            fontFamily: 'Nunito',
+                            fontWeight: FontWeight.bold),
+                        textAlign: TextAlign.center),
+                    SizedBox(height: 20),
+                    Text(
+                      "Aqui as suas ideias são salvadoras.",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: 'Nunito',
+                          fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                   ElevatedButton(
+                  onPressed: () {
+                    // Add your action here
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color.fromARGB(255, 0, 72, 82),
+                  ),
+                  child: Text("Veja mais", style: TextStyle(color: Colors.white, fontFamily: 'Nunito', )),
+                   )
+                  ],
+                )),
+          ), 
+          //onda,
 
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor:Color.fromARGB(255,0,72,82),
-                    );
-                  }, child: Text("Veja mais"))
-                ],)
-                
-              ),),
+          SizedBox(height: 100),
 
-        ],)),
+          Text("Principais Ideias", style: TextStyle(fontFamily: 'Nunito', fontSize: 30, color: Colors.black, fontWeight: FontWeight.bold),),
+
+            PageView.builder(
+            itemCount: 2,
+            pageSnapping: true,
+            itemBuilder: (context,pagePosition){
+            return Container(
+              margin: EdgeInsets.all(10),
+              child: Image.network(images[pagePosition]));
+          })
+
+          ],
+        )
+      ),
     );
   }
 }
