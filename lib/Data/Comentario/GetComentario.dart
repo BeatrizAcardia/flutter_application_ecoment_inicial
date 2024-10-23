345809a2eefccfc519817f4537458c1d27214556
VHSNWLF <cl202247@g.unicamp.br>
2024-10-23 08:37:44 -0300

    Comentarios funcionando com BD

---
+++ lib/Data/Comentario/GetComentario.dart
@@ -0,0 +1,25 @@
+// ignore_for_file: unnecessary_brace_in_string_interps
+
+import 'package:flutter_application_ecoment_inicial/Data/dados.dart';
+import 'package:flutter_application_ecoment_inicial/models/Comentarios.dart';
+import 'package:flutter_application_ecoment_inicial/repository/ComentarioRepository.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class GetComentario{
+  Dados dados = Dados();
+
+  GetComentario();
+
+    Future<List<Comentarios>> listaComentarios(int idPostagem) async{
+    var url = Uri.parse("http://${dados.ipMaquina}:${dados.porta}/Ecomoment/comentario/ByIdPostagem/${idPostagem}");
+    http.Response response = await http.get(url);
+    if(response.statusCode == 200){
+        ComentarioRepository comentarioRepo = ComentarioRepository();
+        List listaComentarios = jsonDecode(response.body) as List;
+        return comentarioRepo.listaComentarios = listaComentarios.map((e) => Comentarios.fromJson(e)).toList();
+    }
+    return [];
+  }
+
+}
